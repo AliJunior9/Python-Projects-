d551c3da7badaad8acfefd3427763db147a46fc1
Ali Junior <wadaiko234@gmail.com>
2026-01-01 17:32:32 +0100

    Update first code.py

--- first code.py
+++ first code.py
@@ -3,4 +3,4 @@ age =int(input (" How old are you? : "))
 if age >= 18:
   print(f"Dear {name} ,Welcome you are eligible ")
 else :
- print(f"Dear {name} , sorry you are not eligible ")
+  print(f"Dear {name} , sorry you are not eligible ")
