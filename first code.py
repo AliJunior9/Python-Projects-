d977ebca2dc46cfbd2298b152525a00339fcfe1f
Ali Junior <wadaiko234@gmail.com>
2026-01-01 17:30:44 +0100

    Create first code.py

---
+++ first code.py
@@ -0,0 +1,6 @@
+name = input("What is your name ?: ")
+age =int(input (" How old are you? : "))
+if age >= 18:
+  print(f"Dear {name} ,Welcome you are eligible ")
+else :
+  print(f"Dear {name} , sorry you are not eligible ")
